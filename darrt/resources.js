e44122548ba2eab2db99cd533927c9ef8b25c434
Mike Amundsen <mamund@yahoo.com>
2020-03-05 16:12:47 -0800

    update resource module

--- darrt/resources.js
+++ darrt/resources.js
@@ -45,63 +45,117 @@ var metadata = [
 
 // home
 router.get('/',function(req,res){
-  utils.handler(req,res,actions.home,"home", 
-    {
-      metadata:metadata,
-      templates:templates,
-      forms:forms,
-      filter:"home"
-    }
-  )
+  var args = {};
+  args.request = req;
+  args.response = res;
+  args.action = actions.home;
+  args.type = "home";
+  args.config = {
+    metadata:metadata,
+    templates:templates,
+    forms:forms,
+    filter:"home"
+  };
+  respond(args);
 });
 
 // credit check list
 router.get('/list/',function(req,res){
-  utils.handler(req,res,actions.list,"credit", 
-    {
-      metadata:metadata,
-      templates:templates,
-      forms:forms,
-      filter:"list"
-    }
-  )
+  var args = {};
+  args.request = req;
+  args.response = res;
+  args.action = actions.list;
+  args.type = "credit";
+  args.config = {
+    metadata:metadata,
+    templates:templates,
+    forms:forms,
+    filter:"list"
+  };
+  respond(args);
 });
 
 // credit check item
 router.get('/list/:id', function(req,res){
-  utils.handler(req,res,actions.item,"credit", 
-    {
-      metadata:metadata,
-      templates:templates,
-      forms:forms, 
-      filter:"item"
-    }
-  )
+  var args = {};
+  args.request = req;
+  args.response = res;
+  args.action = actions.item;
+  args.type = "credit";
+  args.config = {
+    metadata:metadata,
+    templates:templates,
+    forms:forms,
+    filter:"item"
+  };
+  respond(args);
 });
 
 // credit check form READ
 router.get('/form/', function(req,res){
-  utils.handler(req,res,actions.form,"credit", 
-    {
-      metadata:metadata,
-      templates:templates,
-      forms:forms, 
-      filter:"item"
-    }
-  )
+  var args = {};
+  args.request = req;
+  args.response = res;
+  args.action = actions.form;
+  args.type = "credit";
+  args.config = {
+    metadata:metadata,
+    templates:templates,
+    forms:forms,
+    filter:"item"
+  }
+  respond(args);
 });
 
 // credit check form CREATE
 router.post('/form/', function(req,res){
-  utils.handler(req,res,actions.create,"credit", 
-    {
-      metadata:metadata,
-      templates:templates,
-      forms:forms,
-      filter:"item"
-    }
-  )
+  var args = {};
+  args.request = req;
+  args.response = res;
+  args.action = actions.create;
+  args.type = "credit";
+  args.config = {
+    metadata:metadata,
+    templates:templates,
+    forms:forms,
+    filter:"item"
+  };
+  respond(args);
 });
+
+/***********************************************************************/
+
+// initialize module
+function init() {
+  express = require('express')
+  router = express.Router()
+  bodyParser = require('body-parser');
+
+  actions = require('./actions');
+  representation = require('./representation');
+  transitions = require('./transitions');
+  utils = require('./lib/utils');
+
+  // set up request body parsing & response templates
+  router.use(bodyParser.json({type:representation.getResponseTypes()}));
+  router.use(bodyParser.urlencoded({extended:representation.urlencoded}));
+
+  // load response templates and input forms
+  templates = representation.getTemplates();
+  forms = transitions.forms;
+}
+
+// local resource handler function
+function respond(args) {
+  var request = args.request||null;
+  var response = args.response||null;
+  var action = args.action||null;
+  var object = args.type||"";
+  var config = args.config||{};
+
+  return utils.handler(request,response,action,object,config);	
+}
 
 // publish the capability routes
 module.exports = router
+
